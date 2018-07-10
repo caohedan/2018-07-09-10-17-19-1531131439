403ff7a17ad096181b0b454334a66c920aca8213
caohedan <m18390990364@163.com>
2018-07-10 13:43:26 +0800

    'finishpractice-04with6test'

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,18 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    String name;
+    int age;
+    int klass;
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public String introduce () {
+        return super.introduce()+" I am a Student. I am at Class "+this.klass+".";
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
